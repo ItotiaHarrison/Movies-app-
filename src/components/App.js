12eb568d640aa173cc5cc602b85f5f9250f6598d
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-16 00:12:19 +0300

    delete intro

--- src/components/App.js
+++ src/components/App.js
@@ -45,7 +45,7 @@ function App() {
     <div className="App">
       <Header text="MOVIES" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
+      
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading...</span>
