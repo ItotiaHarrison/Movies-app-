2f0ba730f096e7da3fae24fa98c78a554246c0ee
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-15 23:16:14 +0300

    import component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Header from './Header';
+import Movie from './Movie';
+import Search from './Search';
 
 function App() {
   return (
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Search(){
-  return (
-    <div>Search</div>
-  )
+function Search(props) {
+    const [searchValue, setSearchValue] = useState('')
+
+    return (
+        <form className="search">
+            <input
+                value={searchValue}
+                onChange={handleSearchInputChanges}
+                type="text"
+            />
+            <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+        </form>
+    )
 }
 
 export default Search
